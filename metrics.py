b800dad2c67f1cae75d81f118530e36bf56a34ac
Sarthak-100 <sarthak20576@iiitd.ac.in>
2024-05-13 12:24:10 +0530

    Added framework diagrams

--- metrics.py
+++ metrics.py
@@ -1,12 +1,12 @@
-def compute_metrics(Mpred, Mref):
+def compute_metrics(Ref_len, Pred_len, Inter_len):
     # Calculate the intersection of Mpred and Mref for true positives
-    true_positives = len(Mpred.intersection(Mref))
+    true_positives = Inter_len
 
     # Precision: |Mpred ∩ Mref| / |Mpred|
-    precision = true_positives / len(Mpred)
+    precision = true_positives / Pred_len
 
     # Recall: |Mpred ∩ Mref| / |Mref|
-    recall = true_positives / len(Mref)
+    recall = true_positives / Ref_len
 
     # F1 Score: 2 * (Precision * Recall) / (Precision + Recall)
     f1_score = 2 * (precision * recall) / (precision + recall)
